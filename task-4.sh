a89efff587f5731ce79eaa2a3f7f064f622d27fa
manar <manarmohamed.icloud.com@gmail.com>
2025-04-22 23:48:20 +0200

    task-4.sh

---
+++ task-4.sh
@@ -0,0 +1,6 @@
+mkdir -p  egypt_cities/{Alexandria,Cairo,PortSaid,Delta}
+ touch egypt_cities/Alexandria/data.txt
+ touch egypt_cities/Cairo/data.txt
+touch egypt_cities/PortSaid/data.txt
+touch egypt_cities/Delta/data.txt
+rm -rf egypt_cities
